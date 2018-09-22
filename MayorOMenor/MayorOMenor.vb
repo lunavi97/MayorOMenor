a188a59c4424751d08cd844a68f7e474899d0eef
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-22 02:33:23 -0300

    Mostrada victoria o derrota con un mensaje

--- MayorOMenor/MayorOMenor.vb
+++ MayorOMenor/MayorOMenor.vb
@@ -1,8 +1,11 @@
 ﻿Public Class MayorOMenor
     Private bits As Integer
     Private numeroGenerado As Integer
+    Private minGenerador, maxGenerador As Integer
 
     Private Sub MayorOMenor_Load(sender As Object, e As EventArgs) Handles Me.Load
+        minGenerador = 1
+        maxGenerador = 100
         bits = 1000
         ChangeBitsLabel(bits)
         CantidadApostar.Minimum = 1
@@ -22,4 +25,28 @@
     Private Sub ChangeTextOfGroupBox(num As Integer)
         GroupBox.Text = "¿El número siguiente es mayor o menor a " + Format(num) + "?"
     End Sub
+
+    Private Sub ApostarButton_Click(sender As Object, e As EventArgs) Handles ApostarButton.Click
+        If CantidadApostar.Value > bits Then
+            Return
+        End If
+
+        If CantidadApostar.Value < 1 Then
+            Return
+        End If
+
+        Dim numeroAComparar As Integer = numeroGenerado
+        numeroGenerado = GenerarNumeroAleatorio(minGenerador, maxGenerador)
+        If numeroGenerado > numeroAComparar And MayorSel.Checked Or
+                numeroGenerado < numeroAComparar And MenorSel.Checked Then
+            MsgBox("Ganaste " + Format(CantidadApostar.Value) + " bits")
+        Else
+            MsgBox("Perdiste " + Format(CantidadApostar.Value) + " bits")
+        End If
+    End Sub
+
+    Private Function GenerarNumeroAleatorio(min As Integer, max As Integer) As Integer
+        Randomize()
+        Return CInt(Int((max * Rnd()) + min))
+    End Function
 End Class
